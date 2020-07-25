b1f177add82ed239cd5f6b0e6503fd2a40044b30
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-25 09:21:52 +0300

    create title

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 
+// import d3 package
+import * as d3 from 'd3'
+
+// create container
+const container = d3.select('#container')
+
+  // Create the titel
+container.append('h2')
+  .attr('id', 'title')
+  .text('Gross Domestic Product')
+
+// define margins
+
+// define dimensions
+
+// create the svg canvas
+
+// create a group for svg elements
+
+// define scales
+
+// define helper functions
